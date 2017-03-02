585ec44260050763b112eda230292407a4a1ff5e
Zach Silveira <zackify@gmail.com>
2017-03-01 18:32:20 -0800

    Add memoize

---
+++ src/memoize.js
@@ -0,0 +1,122 @@
+//
+// Taken from https://github.com/caiogondim/fast-memoize.js because it does not offer a umd build at this time
+//
+
+export default function memoize (fn, options) {
+  var cache
+  var serializer
+  var strategy
+
+  if (options && options.cache) {
+    cache = options.cache
+  } else {
+    cache = cacheDefault
+  }
+
+  if (options && options.serializer) {
+    serializer = options.serializer
+  } else {
+    serializer = serializerDefault
+  }
+
+  if (options && options.strategy) {
+    strategy = options.strategy
+  } else {
+    strategy = strategyDefault
+  }
+
+  return strategy(fn, {
+    cache,
+    serializer
+  })
+}
+
+//
+// Strategy
+//
+
+function isPrimitive (value) {
+  return value == null || (typeof value !== 'function' && typeof value !== 'object')
+}
+
+function strategyDefault (fn, options) {
+  function monadic (fn, cache, serializer, arg) {
+    var cacheKey
+    if (isPrimitive(arg)) {
+      cacheKey = arg
+    } else {
+      cacheKey = serializer(arg)
+    }
+
+    if (!cache.has(cacheKey)) {
+      var computedValue = fn.call(this, arg)
+      cache.set(cacheKey, computedValue)
+      return computedValue
+    }
+
+    return cache.get(cacheKey)
+  }
+
+  function variadic (fn, cache, serializer, ...args) {
+    var cacheKey = serializer(args)
+
+    if (!cache.has(cacheKey)) {
+      var computedValue = fn.apply(this, args)
+      cache.set(cacheKey, computedValue)
+      return computedValue
+    }
+
+    return cache.get(cacheKey)
+  }
+
+  var memoized = fn.length === 1
+    ? monadic
+    : variadic
+
+  memoized = memoized.bind(
+    this,
+    fn,
+    options.cache.create(),
+    options.serializer
+  )
+
+  return memoized
+}
+
+//
+// Serializer
+//
+
+function serializerDefault (...args) {
+  return JSON.stringify(args)
+}
+
+//
+// Cache
+//
+
+class ObjectWithoutPrototypeCache {
+  constructor () {
+    this.cache = Object.create(null)
+  }
+
+  has (key) {
+    return (key in this.cache)
+  }
+
+  get (key) {
+    return this.cache[key]
+  }
+
+  set (key, value) {
+    this.cache[key] = value
+  }
+
+  delete (key) {
+    delete this.cache[key]
+  }
+}
+
+const cacheDefault = {
+  create: () => new ObjectWithoutPrototypeCache()
+}
