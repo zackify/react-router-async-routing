1027170a699eefddff910b3c1b5ad9da3f92c627
Zach Silveira <zackify@gmail.com>
2017-03-03 09:54:09 -0800

    fix naming

--- example/routing.js
+++ example/routing.js
@@ -1,4 +1,4 @@
 import routes from './routes'
 import AsyncSetup from '../src'
 
-export AsyncSetup(routes, path => import(`../views/${page}.js`))
+export AsyncSetup(routes, path => import(`../views/${path}.js`))
